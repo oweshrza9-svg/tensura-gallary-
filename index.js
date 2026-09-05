1140e7cfa1298fa892eac10401ac7f2e82591705
oveshrza9-svg <oweshrza9@gmail.com>
2026-09-05 21:32:33 +0530

    the bg color

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   direction: 'vertical',
-//   loop: true,
-
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: '.swiper-scrollbar',
-//   },
-// });
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  spaceBetween: 20,
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    650: { slidesPerView: 2 },
+    900: { slidesPerView: 3 },
+    1100: { slidesPerView: 4 },
+  },
+});
